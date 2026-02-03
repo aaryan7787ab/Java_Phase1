4012094f05df80d216a5d335f7c25aa2d9776403
Aryan Bhardwaj <baaryan428@gmail.com>
2026-02-03 19:18:35 +0530

    Add files via upload

---
+++ helloworld.java
@@ -0,0 +1,8 @@
+// this is my first code in java language ..
+class helloworld{
+    public static void main(String[] args){
+            System.out.println("Hello World!");
+            String name = "Aryan Bhardwaj";
+            System.out.println("My name is "+name);
+    }
+}
